805c4f0e611b981763ca2f0a4d74edbf426f7944
Paulinahphetla <motlabilepaulinah@gmail.com>
2023-04-17 14:45:51 +0200

    added a picture

---
+++ FirstConsoleApp2023/helloWorld.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsoleApp2023
+{
+    internal class helloWorld
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hello world");
+            Console.ReadLine();
+        }
+    }
+}
